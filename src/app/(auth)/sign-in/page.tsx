e99e671bb3f15da44490a9215aa43e8ec19d4359
surajgoraicse <surajgoraicse@gmail.com>
2025-02-02 11:21:12 +0530

    sign-in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,117 @@
-import React from 'react'
+"use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useState } from "react";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { signInSchema } from "@/schemas/signInSchema";
+
+// will use usehooks-ts for debouncing to check if the username exists in db.
 function page() {
-  return (
-    <div>page</div>
-  )
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const { toast } = useToast();
+	const router = useRouter();
+
+	// zod implementation
+	const form = useForm<z.infer<typeof signInSchema>>({
+		resolver: zodResolver(signInSchema),
+		defaultValues: {
+			identifier: "",
+			password: "",
+		},
+	});
+
+	const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+		const result = await signIn("credentials", {
+			redirect: false,
+			identifier: data.identifier,
+			password: data.password,
+		});
+		console.log("result data from signin page: ", result);
+		if (result?.error) {
+			console.log("sign in error : result data ", result);
+			toast({
+				title: "Login Failed",
+				description: "Incorrect username or password",
+				variant: "destructive",
+			});
+		}
+		// we will not redirect by the default nextauth redirect rather than we will do it manually
+		if (result?.url) {
+			router.replace("/dashboard");
+		}
+	};
+
+	return (
+		<div className="flex justify-center items-center min-h-screen bg-gray-100">
+			<div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+				<div className="text-center">
+					<h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+						Join Mystery Message
+					</h1>
+					<p className="mb-4">Sign in to start your anonymous adventure</p>
+				</div>
+				<Form {...form}>
+					<form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+						<FormField
+							name="identifier"
+							control={form.control}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email/Username</FormLabel>
+									<FormControl>
+										<Input placeholder="email/username" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							name="password"
+							control={form.control}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<FormControl>
+										<Input type="password" placeholder="password" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<Button type="submit" disabled={isSubmitting}>
+							Signin
+						</Button>
+					</form>
+				</Form>
+				<div className="text-center" mt-4>
+					<p>
+						Register here{" "}
+						<Link
+							className="text-blue-600 hover:text-blue-800"
+							href={"/sign-up"}
+						>
+							Sign-up
+						</Link>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default page
+export default page;
